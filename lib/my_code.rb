444b07a549194210eb345c830c6190e0d12ab461
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-05 21:29:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(array)
   final_array = []
   counter = 0 
   while counter < array.size do 
-    final_array << array[counter]
+    final_array << array[counter] * -1
   end
 end
 
